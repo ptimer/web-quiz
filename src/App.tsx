1f8ca794d10e298a0742da400af176aafc2d7013
Illia Bilous <bilous.job@yahoo.com>
2025-06-19 15:28:37 +0300

    style: all

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-50">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline text-red-50">Hello world!</h1>
     </>
   )
 }
--- src/common/types.d.ts
+++ src/common/types.d.ts
@@ -1,54 +1,47 @@
-type Currency = 'INR' | 'USD' | 'EUR' | 'GBP';
+type Currency = 'INR' | 'USD' | 'EUR' | 'GBP'
 
 interface MarketIndex {
-  name: string;
-  currentValue: number;
-  currency: Currency;         
-  percentChange: number;
+  name: string
+  currentValue: number
+  currency: Currency
+  percentChange: number
 }
 
 interface Quiz {
-  id: string;
-  name: string;
+  id: string
+  name: string
 }
 
 interface QuizQuestions {
-  quizId: string;
-  questionIds: string[];
+  quizId: string
+  questionIds: string[]
 }
 
 interface Option<T> {
- id: string;
- data: T;
+  id: string
+  data: T
 }
 
-type OptionVariant = 'marketIndex' | 'text';
+type OptionVariant = 'marketIndex' | 'text'
 
 interface BaseOptionData {
-  variant: OptionVariant;
+  variant: OptionVariant
 }
 
 interface TextOptionData extends BaseOptionData {
-  variant: 'text';
-  text: string;
+  variant: 'text'
+  text: string
 }
 
-interface MarketIndexOptionData extends BaseOptionData, MarketIndex  {
-  variant: 'marketIndex';
+interface MarketIndexOptionData extends BaseOptionData, MarketIndex {
+  variant: 'marketIndex'
 }
 
-type OptionData = MarketIndexOptionData | TextOptionData;
+type OptionData = MarketIndexOptionData | TextOptionData
 
 interface Question<T = OptionData> {
- id: string;
- text: string;
- options: Option<T>[];
- correctAnswerId: string;
+  id: string
+  text: string
+  options: Option<T>[]
+  correctAnswerId: string
 }
-
-
-
-
-
-
-
