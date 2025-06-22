289c3515d8eacc6e8c6905292dc68d2d6a4be040
Illia Bilous <bilous.job@yahoo.com>
2025-06-22 23:48:06 +0300

    sdfd

--- src/components/QuizScoreBadge.tsx
+++ src/components/QuizScoreBadge.tsx
@@ -2,7 +2,7 @@ export const QuizScoreBadge = ({ score }: { score: number }) => {
   return (
     <div className="flex justify-between bg-light px-8 lg:px-13 py-4 lg:py-6 gap-4 lg:gap-6 rounded-[0.25rem] lg:rounded-[0.375rem] min-w-60 lg:min-w-90">
       <img
-        src="icons/banknotes.png"
+        src="icons/banknotes.svg"
         className="w-14 h-14 lg:w-22 lg:h-22"
         width={14}
         height={14}
